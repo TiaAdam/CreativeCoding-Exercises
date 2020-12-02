b269eb3c6c77e18164e3d7a3c38d777d01979e94
TiaAdam <tijanelooadam123@gmail.com>
2020-12-02 21:11:52 +0000

    Tidying sketch.js for Exercise 2.1

--- Exercise 2.1 - Shape and Pattern/sketch.js
+++ Exercise 2.1 - Shape and Pattern/sketch.js
@@ -5,28 +5,32 @@ let numOfSegments = 20;
 let stepAngle = 360/numOfSegments;
 let radius = 200;
 
+// This function runs once. In here, we set up a Canvas.
 function setup(){
 createCanvas(500,500);
-colorMode(HSB,360,100,100);
+colorMode(HSB,360,100,100); // Hue,Saturation and Boldness.
 angleMode(RADIANS);
 noStroke();
 
 }
 
 function draw(){
+background(mouseY/2, 100,100);
 numOfSegments = map(mouseY,0,400,12,180);
 stepAngle = 360/numOfSegments;
-// Allows us to increase and decrease radius as we move the mouse
+
+// Allows us to increase and decrease radius as we move the mouse.
 radius = map(mouseX,0,400,50,200);
 
-background(mouseY/2, 100,100);
-fill(360-mouseY/2,100,100);
-ellipse(height/2,height/2, mouseX+1,mouseX+1); // x,y,width,height
 
-} //end of draw function
+fill(360-mouseY/2,100,100);
+ellipse(height/2,height/2, mouseX+1,mouseX+1); // x,y,width,height.
+
+} // End of draw function
+
 
 function keyPressed(){
   if(key =='s' || key =="S"){
-    saveCanvas(gd.timestamp(),'png'); // allows us to take a screenshot
+    saveCanvas(gd.timestamp(),'png'); // Allows us to take a screenshot
   }
-} // end of keyPressed function
+} // Wnd of keyPressed function
