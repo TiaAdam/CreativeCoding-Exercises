b59dad2e6c145797f264387abdcc5975f7dc33c0
TiaAdam <tijanlooadam123@gmail.com>
2021-01-19 15:57:43 +0100

    Fixing code and adding lecturer's example as a future reference.

--- Random Agent Exercise/sketch.js
+++ Random Agent Exercise/sketch.js
@@ -1,13 +1,14 @@
 let direction;
-let circleXpos = 250;
+let circleXpos = 250; // tracking the position of the object
 let circleYpos = 250;
 
+// let stepSize = 5; // We can use it for moving the object.
 
  function setup() {
    createCanvas(500,500);
    background(0);
-
  }
+
 
  function draw() {
   ellipse(circleXpos,circleYpos,25,25);
@@ -21,5 +22,15 @@ let circleYpos = 250;
     circleXpos = circleXpos;
   }
   //console.log(direction);
-  
+
 } // End of draw function
+
+
+// 19.1.2021
+// John's code
+// Normalization
+// let randomNum(X or Y) = round(random(-1,1));
+// let moveX = randomNumX * stepSize;
+// let moveY = randomNumY * stepSize;
+// xPos += moveX;
+// yPos += moveY;
