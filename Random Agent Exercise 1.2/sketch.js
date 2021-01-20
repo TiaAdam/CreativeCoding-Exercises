cd9934b9a5f4708c6658d046bd0ae63491fd67be
TiaAdam <tijanlooadam123@gmail.com>
2021-01-20 11:23:50 +0100

    Adding keyReleased function and checkBoundaries function.

--- Random Agent Exercise 1.2/sketch.js
+++ Random Agent Exercise 1.2/sketch.js
@@ -1,9 +1,10 @@
-let direction;
+//let direction;
 let xPos = 250; // tracking the position of the object
 let yPos = 250;
 let stepSize = 5; // We can use it for moving the object.
 let optionsX = [-1,-1, 0, 1, 1, 1, 1];
 let optionsY = [-1, 0, 1];
+let drawMode;
 
  function setup() {
    createCanvas(500,500);
@@ -19,7 +20,7 @@ let optionsY = [-1, 0, 1];
      noStroke();
      //let randomNumX = round(random(0, optionsX.length));
      //let randomNumY = round(random(0, optionsY.length));
-     
+
       let moveX = optionsX[floor(random(0, optionsX.length))] * stepSize;
       let moveY = optionsX[floor(random(0, optionsY.length))] * stepSize;
       //let moveX = randomNumX * stepSize;
@@ -30,5 +31,36 @@ let optionsY = [-1, 0, 1];
       ellipse(xPos, yPos, 5, 5);
 
   } // End of Loop
-
+  checkBoundaries();
 } // End of draw function
+
+function checkBoundaries() {
+  if(yPos < 500 || yPos < 0) {
+    yPos = 500 / 2;
+  }
+
+  if (xPos > 500 || xPos < 0) {
+    xPos = 0;
+  }
+}
+
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  if (keyCode == DELETE || keyCode == BACKSPACE) clear();
+
+  if (key == '1'){
+    drawMode = 1;
+    stepSize = 1;
+    diameter = 1;
+  }
+  if (key == '2') {
+    drawMode = 2;
+    stepSize = 1;
+    diameter = 1;
+  }
+  if (key == '3') {
+    drawMode = 3;
+    stepSize = 10;
+    diameter = 5;
+  }
+}
