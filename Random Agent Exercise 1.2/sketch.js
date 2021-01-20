14263133dd66ed304f8dab4cfa7d8027c39665cd
TiaAdam <tijanlooadam123@gmail.com>
2021-01-20 10:44:01 +0100

    Adding an Array of options.

--- Random Agent Exercise 1.2/sketch.js
+++ Random Agent Exercise 1.2/sketch.js
@@ -2,6 +2,8 @@ let direction;
 let xPos = 250; // tracking the position of the object
 let yPos = 250;
 let stepSize = 5; // We can use it for moving the object.
+let optionsX = [-1, 0, 1,1];
+let optionsY = [-1, 0, 1,1];
 
  function setup() {
    createCanvas(500,500);
@@ -13,15 +15,20 @@ let stepSize = 5; // We can use it for moving the object.
    let diam = map(mouseY, 0, width, 1, 10);
 
    for(let x = 0; x < speed; x++){
-    fill(255,10);
-    noStroke();
-     let randomNumX = round(random(-1,1));
-     let randomNumY = round(random(-1,1));
-     let moveX = randomNumX * stepSize;
-      let moveY = randomNumY * stepSize;
+     fill(250,10);
+     noStroke();
+     let randomNumX = round(random(0,3));
+     let randomNumY = round(random(0,3));
+
+      let moveX = optionsX[randomNumX] * stepSize;
+      let moveY = optionsX[randomNumY] * stepSize;
+      //let moveX = randomNumX * stepSize;
+      //let moveY = randomNumY * stepSize;
+
       xPos += moveX;
       yPos += moveY;
       ellipse(xPos, yPos, 5, 5);
-  }
+
+  } // End of Loop
 
 } // End of draw function
