353e73562fe8432061a31837332413e2c7a183da
TiaAdam <tijanlooadam123@gmail.com>
2021-02-02 13:44:37 +0100

    Updating Random Agent part 3.

--- Random Agent Exercise part 3 - Circle/README.md
+++ Random Agent Exercise part 3 - Circle/README.md
@@ -5,7 +5,7 @@ Random Agent Exercise 1.3 - Circle - Term 2
 
 - This Exercise folder contains code that is similar to previous Random Agent Exercises but this time we worked on the random movement of circle.
 
-- In this Exercise, we were required to make object move in 'random' direction (based on optionsX and optionsY provided).
+- In this Exercise, we were required to make object move in 'random' direction (based on optionsX and optionsY provided). Basically to make circle bounce around the canvas at the random angle.
 
 - We also had to use Sin and Cos, for moving circle randomly across the Canvas.
 
--- Random Agent Exercise part 3 - Circle/sketch.js
+++ Random Agent Exercise part 3 - Circle/sketch.js
@@ -1,43 +1,52 @@
 
 let xPos = 400; // tracking the position of the object
 let yPos = 400;
-let stepSize = 5; // We can use it for moving the object.
-angle = 45
+let stepSize = 1; // We can use it for moving the object.
+let angle = 45
 
  function setup() {
    createCanvas(800,800);
    background(0);
-   angleMode(RADIANS);
+   //angleMode(RADIANS);
  }
 
  function draw() {
-   let speed = map(mouseX,0, width, 2, 10);
+   let speed = map(mouseX,0, width, 2, 40);
 
-   for(let x = 0; x < speed; x++){
+   for(let x = 0; x < 10; x++){
      stroke(255);
-     strokeWeight(10);
+     strokeWeight(5);
      point(xPos,yPos);
 
      // This code will move circle across the screen
      xPos += cos(radians(angle)) * stepSize;
      yPos += sin(radians(angle)) * stepSize;
 
+     if (xPos < 0 || xPos > width || yPos < 0 || yPos > height) {
+angle = deflectAngle(xPos, yPos);
+}
+
   } // End of Loop
 
 } // End of draw function
 
+function deflectAngle(_x, _y) {
+let randomAngle = floor(random(0, 180));
 
-function randomAngle(posX, posY) {
-  if(posX < 0){
-    return floor(random(90, 270));
-  }
-  if(posX > width){
-    return floor(random(90,270));
-  }
-  if(posY < 0 ){
-    return floor(random(0,180));
-  }
-  if(posY > height){
-    return floor(random(0,180))
-  }
+if (_x > width) {
+return randomAngle + 90;
+} else if (_x < 0) {
+return randomAngle + 270;
+}
+
+if (_y > height) {
+return randomAngle + 180;
+
+} else if (_y < 0) {
+return randomAngle;
+}
+}
+
+function keyPressed() {
+if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
 }
