0a706b2d08a14f6aa7171b301ff43cec5bae5cba
TiaAdam <tijanlooadam123@gmail.com>
2021-02-03 10:45:45 +0100

    Tidying sketch.js

--- Curves/sketch.js
+++ Curves/sketch.js
@@ -1,25 +1,26 @@
 // Curves 03.02.2021
 
 let points = [];
-let numOfSegments = 6;
+let numOfSegments = 16;
 let radius = 100;
 
 function setup() {
-  createCanvas(500,500);
+  createCanvas(500, 500);
   background(0);
 
-  let angle = radians(360/numOfSegments);
-  for(let i=0; i<numOfSegments; i++){
-    let xPos = cos(angle*i) * radius;
-    let yPos = sin(angle*i) * radius;
-    points.push(createVector(xPos,yPos));
+  let angle = radians(360 / numOfSegments);
+  for (let i = 0; i < numOfSegments; i++) {
+    let xPos = cos(angle * i) * radius;
+    let yPos = sin(angle * i) * radius;
+    points.push(createVector(xPos, yPos));
 
   }
 }
 
-function draw () {
-  for(let i = 0; i < points.length; i++){
-    fill(255,0,0);
+function draw() {
+  translate(width / 2, height / 2);
+  for (let i = 0; i < points.length; i++) {
+    fill(255, 0, 0);
     ellipse(points[i].x, points[i].y, 10, 5);
   }
 }
