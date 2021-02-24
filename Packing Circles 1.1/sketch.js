2f0b495b18abe4ebb053db59164a9381db6e2faa
TiaAdam <tijanlooadam123@gmail.com>
2021-02-24 11:46:15 +0100

    Commit.

--- Packing Circles 1.1/sketch.js
+++ Packing Circles 1.1/sketch.js
@@ -1,8 +1,8 @@
 // Packing Circles - 24th of February, 2021
 
 let circles = []; // Array which will store circles.
-let width = 500;
-let height = 500;
+//let width = 500;
+//let height = 500;
 
 // Circle class, with all the required parameters for the circle.
 class Circle {
@@ -13,27 +13,40 @@ class Circle {
 }
 
 function setup (){
-  createCanvas(width,height);
+  createCanvas(500,500);
   circles.push(new Circle(250,250,20));
   //noLoop;
 }
 
 function draw (){
   background(0);
-  for (let i = 0; i< circles.length; i++){
+  for (let i = 0; i < circles.length; i++){
     let packingCircle = circles[i];
-    stroke(255,0,0);
+    stroke(0,255,0);
     noFill();
     ellipse(packingCircle.position.x, packingCircle.position.y, packingCircle.radius*2, packingCircle.radius*2);
 
   }
+  generateCircles();
 }
 
-// Showing created circle function.
-Circle.prototype.show = function() {
-  noFill();
-  strokeWeight(1.5);
-  stroke(255, 0, 175, 225);
-  ellipse(this.x, this.y, this.r * 2);
-
+// This function will add more circles to the array 'circles' and display them on the canvas.
+function generateCircles() {
+  let newCircle = new Circle(random(width), random(height), 10);
+  // To check if circle can be drawn at certain spot.
+  for(let i = 0; i < circles.length; i++){
+    let anotherCircle = circles[i];
+    let distance = dist(newCircle.position.x, newCircle.position.y, anotherCircle.position.x, anotherCircle.position.y);
+    if(distance < anotherCircle.radius + 4) {
+      newCircle = undefined;
+      break;
+    }
+  }
+  // If circle is able to display on certain spot
+  if(newCircle){
+    circles.push(newCircle);
+    return true;
+  } else {
+    return false;
+  }
 }
