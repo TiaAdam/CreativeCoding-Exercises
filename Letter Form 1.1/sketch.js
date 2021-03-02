d83d0159623d6045e8d8745703384608c644669f
TiaAdam <tijanlooadam123@gmail.com>
2021-03-02 15:40:02 +0100

    Updating Libraries

--- Letter Form 1.1/sketch.js
+++ Letter Form 1.1/sketch.js
@@ -1,19 +1,25 @@
 // Letter Form - 2nd of March, 2021
 
+let font;
+let fontPath;
+
 function setup() {
   createCanvas(500, 500);
   noLoop();
-  opentype.load('data/Roboto-Lightitalic.ttf', function(err, f){
-    if(err) {
+  opentype.load("data/Roboto-Lightitalic.ttf", function (err, f) {
+    if (err) {
       console.log(err);
     } else {
       font = f;
       loop();
     }
-    
   });
 }
 
-function draw(){
+function draw() {
+  if (!font) return;
   background(0);
+
+  translate(20, 220);
+  fontPath = font.getPath("Tia", 0, 0, 20);
 }
