1c6a00ab5897fecae4d396dccf5920cbc8e80282
TiaAdam <tijanlooadam123@gmail.com>
2021-03-02 15:24:58 +0100

    Continuing with the project.

--- Letter Form 1.1/sketch.js
+++ Letter Form 1.1/sketch.js
@@ -1,6 +1,19 @@
 // Letter Form - 2nd of March, 2021
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(500, 500);
   noLoop();
+  opentype.load('data/Roboto-Lightitalic.ttf', function(err, f){
+    if(err) {
+      console.log(err);
+    } else {
+      font = f;
+      loop();
+    }
+    
+  });
+}
+
+function draw(){
+  background(0);
 }
