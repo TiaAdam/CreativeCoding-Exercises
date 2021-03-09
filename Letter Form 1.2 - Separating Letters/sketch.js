ec2e6a1bb061ccf978fbfa676ab6671f047e2dcf
TiaAdam <tijanlooadam123@gmail.com>
2021-03-09 14:42:50 +0100

    Adding the use of vertexes in the exercise.

--- Letter Form 1.2 - Separating Letters/sketch.js
+++ Letter Form 1.2 - Separating Letters/sketch.js
@@ -4,10 +4,15 @@ let font;
 let fontPath;
 let path;
 
+// Vertexes that will be used to separate letters from each other
+let beginVertex = 0;
+let endVertex = 0;
+
 // Setup Function
 function setup() {
   createCanvas(800, 800);
   noLoop();
+
   opentype.load('data/Roboto-BoldItalic.ttf', function(err, f) {
     if (err) {
       console.log(err);
@@ -24,14 +29,14 @@ function draw() {
   background(0);
 
   translate(20, 220);
-  fontPath = font.getPath("Hi people!", 100, 300, 100);
+  fontPath = font.getPath("This is Tijana!", 100, 300, 100);
   let pah = new g.Path(fontPath.commands);
   path = g.resampleByAmount(path, 500);
 
 
   beginShape();
   for (let i = 0; i < fontPath.commands.length; i++) {
-    fill(255, 0, 0);
+    fill(255, 255, 100);
     noStroke();
     ellipse(fontPath.commands[i].x, fontPath.commands[i].y);
     vertex(fontPath.commands[i].x, fontPath.commands[i].y);
