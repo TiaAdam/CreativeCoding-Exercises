4adde2f96e8d6b7d82319e66b5ca2d77bb91cecf
TiaAdam <tijanelooadam23@gmail.com>
2021-03-23 11:56:44 +0100

    Displaying circle wheel.

--- CA2 - Generative Agency/sketch.js
+++ CA2 - Generative Agency/sketch.js
@@ -4,41 +4,41 @@
 
 //Variables that will be used in the project.
 let noOfSegments = 20;
-let stepAngle = 360/noOfSegments; // Changing based on noOfSegments variable.
-let radius = 300; // A constant variable - 600/2.
+let stepAngle = 360 / noOfSegments; // Changing based on noOfSegments variable.
+let radius = 200; // A constant variable - 600/2.
 
 // Creates canvas and runs only once.
-function setup (){
-  createCanvas(600,600);
+function setup() {
+  createCanvas(500, 500);
   angleMode(DEGREES); // Need to be specified - RADIANS or DEGREES.
   //noStroke();
   strokeCap(ROUND); // Sets the style for line endings.
 }
 
 // Contains the main code.
-function draw(){
-  background(255);
-  translate(width/2, height/2);
+function draw() {
+  background(0);
+  //translate(width / 2, height / 2);
+  noOfSegments = map(mouseY, 0, 400, 12, 180);
+  stepAngle = 360 / noOfSegments
+  // Increases and decreases radius.
+  radius = map(mouseX, 0, 400, 50, 200);
 
-  noOfSegments = map(mouseY, 0, height,2,80);
-  radius = mouseX - width/2;
-  stepAngle = 360/noOfSegments;
-
-  fill(360,100,100);
+  fill(360, 100, 100);
   push();
   //translate(250,250);
   beginShape(TRIANGLE_FAN);
-  vertex(250,250);
+  vertex(250, 250);
   // For loop - variable a represents alpha.
-  for(let a=0; a<=noOfSegments; a++){
-    let vx= (radius * cos(a)) +300;
-    let vy= (radius *sin(a)) +300;
-    fill(a,100,100);
-
+  for (let a = 0; a <=360; a+=stepAngle) {
+    let vx = (radius * cos(a)) + 250;
+    let vy = (radius * sin(a)) + 250;
+    fill(a, 100, 100);
+    vertex(vx, vy);
   }
   endShape();
   pop();
-
+  console.log("Hello world!");
 }
 
 // KeyPressed function will save an image in png format whenever user presses capital 'S' or lower 's' key.
