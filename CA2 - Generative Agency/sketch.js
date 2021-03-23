2f3a2a54ee722bc4e2dca3d1f92c6f05eb397a70
TiaAdam <tijanelooadam23@gmail.com>
2021-03-23 11:29:59 +0100

    Setting up the sketch.js

--- CA2 - Generative Agency/sketch.js
+++ CA2 - Generative Agency/sketch.js
@@ -1,3 +1,18 @@
 // CA2 - Generative Agency
 // Tijana Adam N001070280
 // Creative Coding Y4
+
+// Creates canvas and runs only once. 
+function setup (){
+  createCanvas(600,600);
+}
+
+function draw(){
+  background(0);
+
+}
+
+// KeyPressed function will save an image in png format whenever user presses capital 'S' or lower 's' key.
+function keyPressed() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+}
